268c1b95770a0eec8d066188ca8b137f76db429d
kaba <fkaba@live.fr>
2021-03-18 16:59:32 +0100

    mise à jour lien images

--- src/main/java/projet/babyShop3/ProjetBabyShop3Application.java
+++ src/main/java/projet/babyShop3/ProjetBabyShop3Application.java
@@ -48,22 +48,22 @@ public class ProjetBabyShop3Application implements CommandLineRunner {
 				categoryRepo.save(new Category("Repas", "https://zupimages.net/viewer.php?id=21/11/wgm5.jpg"));
 				categoryRepo.save(new Category("Eveil", "https://zupimages.net/viewer.php?id=21/11/3oy9.jpg"));
 				
-				productRepo.save(new Product("C001", "Lit", 100, "https://zupimages.net/viewer.php?id=21/11/tpyh.jpg", new Date(),(new Category(1))));
-				productRepo.save(new Product("C002", "Commodes", 100,"https://zupimages.net/viewer.php?id=21/11/bf2b.jpg",new Date(),(new Category(1))));
-				productRepo.save(new Product("C003", "Plan à langer", 100, "https://zupimages.net/viewer.php?id=21/11/bf2b.jpg", new Date(),(new Category(1))));
-				productRepo.save(new Product("C004", "Linge et parrures de lit", 100, "https://zupimages.net/viewer.php?id=21/11/m16d.jpg", (new Date()),(new Category(1))));
-				productRepo.save(new Product("C005", "Décoration", 100, "https://zupimages.net/viewer.php?id=21/11/o455.jpg",(new Date()), new Category(1)));
-				productRepo.save(new Product("C006", "Matelas", 100,"https://zupimages.net/viewer.php?id=21/11/xlig.jpg", new Date(),(new Category(1))));
-				productRepo.save(new Product("C007", "Berceaux et couffins", 100,"https://zupimages.net/viewer.php?id=21/11/68pr.jpg",new Date(),(new Category(1))));
-				productRepo.save(new Product("C008", "Coffre à jouet", 100, "https://zupimages.net/viewer.php?id=21/11/gwjw.jpg",new Date(),(new Category(1))));
-				productRepo.save(new Product("C009", "Securité", 100, "https://zupimages.net/viewer.php?id=21/11/buaf.jpg", new Date(),(new Category(1))));
-				productRepo.save(new Product("C007", "Gigoteuse", 100,"https://zupimages.net/viewer.php?id=21/11/yr3p.jpg",new Date(),(new Category(1))));
+				productRepo.save(new Product("C001", "Lit", 100, "https://zupimages.net/viewer.php?id=21/11/gwjw.jpg", new Date(),(new Category(1))));
+				productRepo.save(new Product("C002", "Commodes", 100,"https://zupimages.net/viewer.php?id=21/11/m16d.jpg",new Date(),(new Category(1))));
+				productRepo.save(new Product("C003", "Plan à langer", 100, "https://zupimages.net/viewer.php?id=21/11/yr3p.jpg", new Date(),(new Category(1))));
+				productRepo.save(new Product("C004", "Linge et parrures de lit", 100, "https://zupimages.net/viewer.php?id=21/11/68pr.jpg", (new Date()),(new Category(1))));
+				productRepo.save(new Product("C005", "Décoration", 100, "https://zupimages.net/viewer.php?id=21/11/tpyh.jpg",(new Date()), new Category(1)));
+				productRepo.save(new Product("C006", "Matelas", 100,"https://zupimages.net/viewer.php?id=21/11/buaf.jpg", new Date(),(new Category(1))));
+				productRepo.save(new Product("C007", "Berceaux et couffins", 100,"https://zupimages.net/viewer.php?id=21/11/bf2b.jpg",new Date(),(new Category(1))));
+				productRepo.save(new Product("C008", "Coffre à jouet", 100, "https://zupimages.net/viewer.php?id=21/11/bf2b.jpg",new Date(),(new Category(1))));
+				productRepo.save(new Product("C009", "Securité", 100, "https://zupimages.net/viewer.php?id=21/11/xlig.jpg", new Date(),(new Category(1))));
+				productRepo.save(new Product("C007", "Gigoteuse", 100,"https://zupimages.net/viewer.php?id=21/11/o455.jpg",new Date(),(new Category(1))));
 				/*
 				productRepo.save(new Product("S001", "Poussette", 100,new Date(),(new Category(2))));
-				productRepo.save(new Product("S002", "Nacelles", 100,new Date(),(new Category(1))));
-				productRepo.save(new Product("S003", "Port-bébé", 100,new Date(),(new Category(1))));
-				productRepo.save(new Product("S004", "Sac à langer", 100,new Date(),(new Category(1))));
-				productRepo.save(new Product("S005", "Habillages pluie", 100,new Date(),(new Category(1))));
+				productRepo.save(new Product("S002", "Nacelles", 100,new Date(),(new Category(2))));
+				productRepo.save(new Product("S003", "Port-bébé", 100,new Date(),(new Category(2))));
+				productRepo.save(new Product("S004", "Sac à langer", 100,new Date(),(new Category(2))));
+				productRepo.save(new Product("S005", "Habillages pluie", 100,new Date(),(new Category(2))));
 				
 				
 				productRepo.save(new Product("R001", "Biberons", 100,(new Category("Repas"))));
